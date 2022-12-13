0fef158c0d611e73366ebd4e59e40be7d3812e9d
Parag Selokar <parag.selokar@ezest.local>
2022-12-13 23:22:34 +0530

    removed unwanted file

--- src/test/java/com/example/demo/service/EmployeeServiceImplTest.java
+++ src/test/java/com/example/demo/service/EmployeeServiceImplTest.java
@@ -22,39 +22,32 @@ public class EmployeeServiceImplTest {
 
 	@InjectMocks
 	EmployeeServiceImpl employeeServiceImpl;
-	
-//	@Mock
-	
+
 	@Mock
 	EmployeeDao employeeDao;
-	
+
 	// JUnit test for getEmpById method
 	@Test
 	public void getEmpByIdTest() {
 		when(employeeDao.getEmpById(5L)).thenReturn(createEmployeeStub());
-		
-		Employee emp = employeeServiceImpl.getEmpById(5L);
-		
+		Employee emp = employeeServiceImpl.getEmpById(5L);		
 		assertEquals(emp.getName(), "name-5");
 	}
-	
-    // JUnit test for getAllEmployees method
-    @Test
+
+	// JUnit test for getAllEmployees method
+	@Test
     public void getAllEmployeesTest(){
-       
     	when(employeeDao.getAllEmployees()).thenReturn(createAllEmployeesStub());
         List<Employee> employeeList = employeeServiceImpl.getAllEmployees();
-
         assertThat(employeeList).isNotNull();
         assertThat(employeeList.size()).isEqualTo(2);
     }
-    
-	
-	private Employee createEmployeeStub(){
+
+	private Employee createEmployeeStub() {
 		Employee emp = new Employee((long) 5, "name-5");
 		return emp;
 	}
-	
+
 	public List<Employee> createAllEmployeesStub() {
 		return IntStream.rangeClosed(1, 2).mapToObj(i -> new Employee((long) i, "name-" + i))
 				.collect(Collectors.toList());
